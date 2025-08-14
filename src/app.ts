e62e1102335bdcc7f779958346c350cb874c99f7
MoisesDuarte <moises.junior@zrp.com.br>
2025-08-14 18:29:21 -0300

    chore: generating minimal working fastify adminjs project

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import AdminJS from 'adminjs'
+import Fastify from 'fastify'
+import * as AdminJSFastify from '@adminjs/fastify'
+
+const PORT = 3000
+
+const start = async () => {
+  const app = Fastify();
+
+  const admin = new AdminJS({
+    rootPath: '/admin',
+    resources: [],
+    databases: [],
+  });
+
+  await AdminJSFastify.buildRouter(
+    admin,
+    app,
+  );
+  
+  app.listen({ port: PORT }, (err, addr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`AdminJS available at http://localhost:${PORT}${admin.options.rootPath}`);
+    }
+  });
+};
+
+start();
